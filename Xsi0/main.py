47e2bbe058ef8b4d5e36e501c1c1d6b76e7ddba1
Zbant Gabriel Alexandru <60919382+zbantalexandru@users.noreply.github.com>
2021-12-03 11:35:26 +0200

    added the smart AI

--- Xsi0/main.py
+++ Xsi0/main.py
@@ -1,3 +1,4 @@
+import numpy.random
 import pygame
 
 
@@ -44,13 +45,13 @@ def addClickBoxes(width, height):
     return res
 
 
-def verifCastig(map):
+def verifStareCastig(map):
     for linie in range(3):
-        if map[linie][0]!=0 and map[linie][0]==map[linie][1]==map[linie][2]:
+        if map[linie][0] != 0 and map[linie][0] == map[linie][1] == map[linie][2]:
             return map[linie][0]
 
     for coloana in range(3):
-        if map[0][coloana] != 0 and map[0][coloana]==map[1][coloana]==map[2][coloana]:
+        if map[0][coloana] != 0 and map[0][coloana] == map[1][coloana] == map[2][coloana]:
             return map[0][coloana]
 
     if map[0][0] == map[1][1] == map[2][2] and map[0][0] != 0:
@@ -60,7 +61,175 @@ def verifCastig(map):
     return 0
 
 
+def addZero(boxes, map, indBox, imagesToDisplay, redSquare, clickedInThePast):
+    contBox = 0
+    map[indBox // 3][indBox % 3] = 2
+    for box in boxes:
+        if contBox == indBox:
+            rect = redSquare.get_rect()
+            rect.width = box.width
+            rect.height = box.height
+            rect.center = box.center
+            imagesToDisplay.append((rect, 2))
+            clickedInThePast.append(box)
+        contBox += 1
+    return -1
+
+
 # def selectSpot(poz,boxes):
+def randomSelect(map):
+    emptySpaces = []
+    for linia in range(3):
+        for coloana in range(3):
+            if map[linia][coloana] == 0:
+                emptySpaces.append(int(3 * linia + coloana))
+    if (len(emptySpaces) > 0):
+        indAles = numpy.random.randint(0, len(emptySpaces) - 1)
+        return emptySpaces[indAles]
+
+
+def cautCastigator(map):
+    castigator = verifStareCastig(map)
+    if castigator == 1:
+        print("Jucatorul a castigat!")
+        winnerFound = 1
+    elif castigator == 2:
+        print("Calculatorul a castigat!")
+        winnerFound = 1
+    elif castigator == 0 and len(availablePositions(map)) == 0:
+        print("Remiza!")
+    if (castigator > 0):
+        return 1
+    else:
+        return 0
+
+
+def availablePositions(map):
+    res = []
+    for linie in range(3):
+        for coloana in range(3):
+            if map[linie][coloana] == 0:
+                res.append((linie, coloana))
+    return res
+
+
+def needsMove(map, player):
+    for linie in range(3):
+        takenSpots = 0
+        emptyLine = -1
+        emptyCol = -1
+        for coloana in range(3):
+            if (map[linie][coloana] == player):
+                takenSpots += 1
+            elif (map[linie][coloana] == 0):
+                emptyLine = linie
+                emptyCol = coloana
+
+        if takenSpots == 2 and emptyCol != -1:
+            return (emptyLine, emptyCol)
+    # testez pe fiecare linie daca pot pierde/pot castiga
+
+    for coloana in range(3):
+        takenSpots = 0
+        emptyLine = -1
+        emptyCol = -1
+        for linie in range(3):
+            if (map[linie][coloana] == player):
+                takenSpots += 1
+            elif (map[linie][coloana] == 0):
+                emptyLine = linie
+                emptyCol = coloana
+
+        if takenSpots == 2 and emptyCol != -1:
+            return (emptyLine, emptyCol)
+    # testez pe fiecare coloana daca pot pierde/pot castiga
+
+    takenSpots = 0
+    lin = -1
+    col = -1
+    for ind in range(3):
+        if map[ind][ind] == player:
+            takenSpots += 1
+        elif map[ind][ind] == 0:
+            lin = ind
+            col = ind
+    if takenSpots == 2 and lin != -1:
+        return (lin, col)
+    #diag principala
+    takenSpots = 0
+    lin = -1
+    col = -1
+    for ind in range(3):
+        if map[ind][2 - ind] == player:
+            takenSpots += 1
+        elif map[ind][2 - ind] == 0:
+            lin = ind
+            col = ind
+    if takenSpots == 2 and lin != -1:
+        return (lin, col)
+    #diagSecundara
+    return (-1, -1)
+
+
+def takeCorner(map):
+    if map[0][0] == 0:
+        return (0, 0)
+    if map[0][2] == 0:
+        return (0, 2)
+    if map[2][0] == 0:
+        return (2, 0)
+    if map[2][2] == 0:
+        return (2, 2)
+    return (-1, -1)
+
+
+def takeCenter(map):
+    if map[1][1] == 0:
+        return (1, 1)
+    else:
+        return (-1, -1)
+
+def takeSides(map):
+    if map[0][1] == 0:
+        return (0, 1)
+    if map[1][0] == 0:
+        return (1, 0)
+    if map[1][2] == 0:
+        return (1, 2)
+    if map[2][1] == 0:
+        return (2, 1)
+    return (-1, -1)
+
+
+def optimalSelect(map):
+    (linie, coloana) = needsMove(map, 2)
+    if (linie, coloana) != (-1, -1):
+        print("linieAI")
+        map[linie][coloana] = 1
+        return linie * 3 + coloana
+    (linie, coloana) = needsMove(map, 1)
+    if (linie, coloana) != (-1, -1):
+        print("linieJucator")
+        map[linie][coloana] = 1
+        return linie * 3 + coloana
+    (linie, coloana) = takeCorner(map)
+    if (linie, coloana) != (-1, -1):
+        print("Colt")
+        map[linie][coloana] = 1
+        return linie * 3 + coloana
+    (linie, coloana) = takeSides(map)
+    if (linie, coloana) != (-1, -1):
+        print("Centru")
+        map[linie][coloana] = 1
+        return linie * 3 + coloana
+    (linie, coloana) = takeCenter(map)
+    if (linie, coloana) != (-1, -1):
+        print("Centru")
+        map[linie][coloana] = 1
+        return linie * 3 + coloana
+
+# def semiRandomSelect(map):
+
 
 def generateMap():
     pygame.init()
@@ -94,13 +263,13 @@ def generateMap():
     map = [[0, 0, 0],
            [0, 0, 0],
            [0, 0, 0]]
-    winnerFound=0
+    stopJoc = 0
     while not crashed:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 crashed = True
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1 and winnerFound==0:
+                if event.button == 1 and stopJoc == 0 and turn % 2 == 1:
                     count = 0
                     for box in boxes:
                         if box.collidepoint(event.pos):
@@ -109,26 +278,20 @@ def generateMap():
                                 rect.width = box.width
                                 rect.height = box.height
                                 rect.center = box.center
-                                if turn % 2 == 1:
-                                    imagesToDisplay.append((rect, 1))
-                                    map[count // 3][count % 3] = 1
-                                else:
-                                    imagesToDisplay.append((rect, 2))
-                                    map[count // 3][count % 3] = 2
-                                castigator = verifCastig(map)
-                                if castigator == 1:
-                                    print("Jucatorul a castigat!")
-                                    winnerFound=1
-                                elif castigator == 2:
-                                    print("Calculatorul a castigat!")
-                                    winnerFound = 1
-                                elif castigator == 0 and turn >= 9:
-                                    print("Remiza!")
-                                    winnerFound = 1
+                                imagesToDisplay.append((rect, 1))
+                                map[count // 3][count % 3] = 1
                                 turn += 1
+                                stopJoc = cautCastigator(map)
                                 clickedInThePast.append(box)
                         count += 1
-
+        if turn % 2 == 0 and turn <= 8 and stopJoc == 0:
+            # spot = randomSelect(map)  # pune random
+            # spot = semiRandomSelect(map)  # pune random1/2 istet 1/2
+            spot = optimalSelect(map)#pune istet
+            print(spot)
+            rect = addZero(boxes, map, spot, imagesToDisplay, redSquare, clickedInThePast)
+            turn += 1
+            stopJoc = cautCastigator(map)
         mainDisplay.fill(grey)
 
         drawLines(mainDisplay, faintGreen, displayWidth, displayHeight, 5)
